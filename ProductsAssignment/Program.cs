7546f7f7edf7a3e546278429870f421dc7d5d218
Maheshvema <babumahesh2088@gmail.com>
2023-11-22 15:17:34 +0530

    modified

---
+++ ProductsAssignment/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductsAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            herbalproduct product = new herbalproduct();
+            product.Set();
+            product.Show();
+            Console.ReadKey();
+        }
+    }
+}
